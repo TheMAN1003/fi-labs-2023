9a4693abf9f11b89e61f10cb2c2d607f4f21ad58
TheMAN1003 <fi.03maksimnedozhdii@gmail.com>
2023-04-03 18:40:09 +0300

    added encoded func and files

--- cp_2/burzhymskyi_fi-03_nedozhdii_fi-03_cp2/lab2.py
+++ cp_2/burzhymskyi_fi-03_nedozhdii_fi-03_cp2/lab2.py
@@ -14,4 +14,21 @@ def redact_text():
     with open('balobol_clean.txt', 'w', encoding="utf8") as f:
         f.write(text)
 
-key = "генацидрусни"
+with open('balobol_clean.txt', 'r', encoding="utf8") as f:
+    text = f.read()
+
+key_2 = "ру"
+key_3 = "чвк"
+key_4 = "груз"
+key_5 = "гойда"
+key_12 = "генацидрусни"
+
+def encode(text, key):
+    code = ""
+    for i in range(len(text)):
+        letter = alphabet[(alphabet.index(text[i]) + alphabet.index(key[i % (len(key))])) % len(alphabet)]
+        code += letter
+    with open('balobol_encoded_12.txt', 'w', encoding="utf8") as f:
+        f.write(code)
+
+
